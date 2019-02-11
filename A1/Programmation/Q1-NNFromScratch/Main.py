7af61c4254f7d271afc491a447f52582238d4b71
CGShocky <charles.huard@usherbrooke.ca>
2019-02-11 17:20:00 -0500

    Cleanup and added tanh and sigmoid non-lin

--- A1/Programmation/Q1-NNFromScratch/Main.py
+++ A1/Programmation/Q1-NNFromScratch/Main.py
@@ -4,16 +4,16 @@ import matplotlib.pyplot as plt
 def main():
     loss_init = []
 
-    MLP = NN((600,600), 784, 10, data_path='mnist.pkl.npy')
+    MLP = NN(784, 10, data_path='mnist.pkl.npy', hidden_dims=(600,600), activation_type='tanh')
 
-    loss_init.append(MLP.train(10, 'zeros'))
+    #loss_init.append(MLP.train(10, 'zeros'))
     loss_init.append(MLP.train(10, 'normal_dist'))
-    loss_init.append(MLP.train(10,'glorot'))
+    #loss_init.append(MLP.train(10,'glorot'))
 
-    plt.plot(loss_init[0], label='Zeros')
-    plt.plot(loss_init[1], label='Dist. Normale')
-    plt.plot(loss_init[2], label='Glorot')
-    plt.legend(loc='upper left')
+    #plt.p#lot(loss_init[0], label='Zeros')
+    #plt.plot(loss_init[1], label='Dist. Normale')
+    #plt.plot(loss_init[2], label='Glorot')
+    #plt.legend(loc='upper left')
 
     plt.show()
     return None
--- A1/Programmation/Q1-NNFromScratch/NN.py
+++ A1/Programmation/Q1-NNFromScratch/NN.py
@@ -3,102 +3,125 @@ import pickle as pickle
 import matplotlib.pyplot as plt
 
 class NN (object):
-    learning_rate = 0.00001
+
+    # Network initialization parameters
     data_path = ''
-    hidden_dims = None
     num_hidden = None
     input_size = 0
     num_class = 0
-    W = []
-    b = []
-    grad = []
-    a1 = []
-    a2 = []
-    o = []
-    l_vector = []
-    plt_vector = []
+
+    # Network hyperparameters
+    hidden_dims = None
+    learning_rate = 0.00001
+    activation_type = ''
     batch_size = 0
 
-    def __init__(self, hidden_dims, input_size, num_class, data_path):
+    # Network parameters
+    params = {
+        'w': [],
+        'b': []
+    }
+
+    # Cache for backpropagation
+    cache = []
+
+    # Utils for loss
+    l_vector = []
+    plt_vector = []
+
+
+    def __init__(self, input_size, num_class, data_path, learning_rate=0.00001, hidden_dims=(600, 600), batch_size=32,
+                 activation_type = 'relu'):
+
         self.data_path = data_path
         self.hidden_dims = hidden_dims
         self.input_size = input_size
         self.num_class = num_class
+        self.learning_rate = learning_rate
         self.num_hidden = len(hidden_dims)
+        self.batch_size = batch_size
+        self.activation_type = activation_type
 
-    def initialize_weights(self, input_size, num_class, init_type='glorot'):
+    def initialize_weights(self, input_size, num_class, init_type):
         #Zero initialization
         if init_type == 'zeros':
-            w1 = np.zeros((input_size, self.hidden_dims[0]))
-            w2 = np.zeros((self.hidden_dims[0], self.hidden_dims[1]))
-            wo = np.zeros((self.hidden_dims[1], num_class))
-            b1 = np.zeros((1,self.hidden_dims[0]))
-            b2 = np.zeros((1,self.hidden_dims[1]))
-            bo = np.zeros((1,num_class))
+            self.params['w'].append(np.zeros((input_size, self.hidden_dims[0])))
+            self.params['w'].append(np.zeros((self.hidden_dims[0], self.hidden_dims[1])))
+            self.params['w'].append(np.zeros((self.hidden_dims[1], num_class)))
+            self.params['b'].append(np.zeros((1,self.hidden_dims[0])))
+            self.params['b'].append(np.zeros((1,self.hidden_dims[1])))
+            self.params['b'].append(np.zeros((1,num_class)))
 
         #Normal distribution initialization(with biases 0)
         if init_type == 'normal_dist':
-            w1 = np.random.normal(0, 0.1, (input_size, self.hidden_dims[0]))
-            w2 = np.random.normal(0, 0.1, (self.hidden_dims[0], self.hidden_dims[1]))
-            wo = np.random.normal(0, 0.1, (self.hidden_dims[1], num_class))
-            b1 = np.zeros((1,self.hidden_dims[0]))
-            b2 = np.zeros((1,self.hidden_dims[1]))
-            bo = np.zeros((1,num_class))
+           self.params['w'].append(np.random.normal(0, 0.1, (input_size, self.hidden_dims[0])))
+           self.params['w'].append(np.random.normal(0, 0.1, (self.hidden_dims[0], self.hidden_dims[1])))
+           self.params['w'].append(np.random.normal(0, 0.1, (self.hidden_dims[1], num_class)))
+           self.params['b'].append(np.zeros((1,self.hidden_dims[0])))
+           self.params['b'].append(np.zeros((1,self.hidden_dims[1])))
+           self.params['b'].append(np.zeros((1,num_class)))
 
         #Glorot initialization
         if init_type == 'glorot':
             d1 = np.sqrt(6./(input_size + self.hidden_dims[0]))
-            w1 = np.random.uniform(-d1, d1, (input_size, self.hidden_dims[0]))
-
             d2 = np.sqrt(6./(self.hidden_dims[0] + self.hidden_dims[1]))
-            w2 = np.random.uniform(-d2, d2, (self.hidden_dims[0], self.hidden_dims[1]))
-
             do = np.sqrt(6./(self.hidden_dims[1] + num_class))
-            wo = np.random.uniform(-do, do, (self.hidden_dims[1], num_class))
 
-            b1 = np.zeros((1,self.hidden_dims[0]))
-            b2 = np.zeros((1,self.hidden_dims[1]))
-            bo = np.zeros((1,num_class))
-
-
-        self.W = [w1, w2, wo]
-        self.b = [b1,b2,bo]
+            self.params['w'].append(np.random.uniform(-d1, d1, (input_size, self.hidden_dims[0])))
+            self.params['w'].append(np.random.uniform(-d2, d2, (self.hidden_dims[0], self.hidden_dims[1])))
+            self.params['w'].append(np.random.uniform(-do, do, (self.hidden_dims[1], num_class)))
+            self.params['b'].append(np.zeros((1,self.hidden_dims[0])))
+            self.params['b'].append(np.zeros((1,self.hidden_dims[1])))
+            self.params['b'].append(np.zeros((1,num_class)))
 
     def forward(self, input):
         #First logit
-        h1 = input.dot(self.W[0]) + self.b[0]
+        h1 = input.dot(self.params['w'][0]) + self.params['b'][0]
 
         #First non-linearity
         a1 = self.activation(h1)
 
         #Second Logit
-        h2 = np.dot(a1, self.W[1]) + self.b[1]
+        h2 = np.dot(a1, self.params['w'][1]) + self.params['b'][1]
 
-        #Second linearity
+        #Second non-linearity
         a2 = self.activation(h2)
 
         #Output logit
-        o = np.dot(a2, self.W[2]) + self.b[2]
+        o = np.dot(a2, self.params['w'][2]) + self.params['b'][2]
 
-        #Saving activations for backprop
-        self.a1 = a1
-        self.a2 = a2
-        self.o = o
+        #Caching activations for backprop
+        self.cache.append(a1)
+        self.cache.append(a2)
 
         #Return the softmax
         return self.softmax(o)
 
 
-    def activation(self, input, type='relu'):
-        if type == 'relu':
+    def activation(self, input):
+        if self.activation_type == 'relu':
             return np.maximum(input, 0, input)
 
-    def relu_derivative(self,x):
-        return 1. * (x > 0)
+        if self.activation_type == 'sigmoid':
+            return 1 / (np.exp(-input) + 1)
 
-    def loss(self, pred_vector, correct_label, type='cross-entropy'):
+        if self.activation_type == 'tanh':
+            return (np.exp(input) - np.exp(-input)) / (np.exp(input) + np.exp(-input))
+
+    def activation_derivative(self, x, num_layer):
+        if self.activation_type == 'relu':
+            return 1. * (x > 0)
+
+        if self.activation_type == 'sigmoid':
+            return self.activation(x)*(1 - self.activation(x))
+
+        if self.activation_type == 'tanh':
+            return 1 - np.square(self.activation(x))
+
+
+    def loss(self, pred_vector, correct_label, batch_size, type='cross-entropy'):
         if type == 'cross-entropy':
-            return np.average(-np.log(pred_vector[range(self.batch_size), correct_label]))
+            return np.average(-np.log(pred_vector[range(batch_size), correct_label]))
 
     def softmax(self, input):
         #Stabilize the softmax
@@ -110,22 +133,22 @@ class NN (object):
 
     def backward(self, input, correct_label, prediction, mini_batch_size):
         #Calculate the loss
-        loss = self.loss(prediction,correct_label)
+        loss = self.loss(prediction,correct_label, mini_batch_size)
         self.l_vector.append(loss)
 
         #Derivative of the output layer ( np.multiply(y, np.multiply(1-y, targets-y)))
         do = prediction
-        do[range(self.batch_size), correct_label] -= 1
-        dwo = np.dot(self.a2.T, do)
+        do[range(mini_batch_size), correct_label] -= 1
+        dwo = np.dot(self.cache[1].T, do)
         bo = np.sum(do,axis=0)
 
         #Derivative of the second layer
-        dh2 = np.dot(do, self.W[2].T) * self.relu_derivative(self.a2)
-        dw2 = np.dot(self.a1.T, dh2)
+        dh2 = np.dot(do, self.params['w'][2].T) * self.activation_derivative(self.cache[1], 2)
+        dw2 = np.dot(self.cache[0].T, dh2)
         b2 = np.sum(dh2,axis=0)
 
         #Derivative of the first layer
-        dh1 = np.dot(dh2, self.W[1].T) * self.relu_derivative(self.a1)
+        dh1 = np.dot(dh2, self.params['w'][1].T) * self.activation_derivative(self.cache[0], 1)
         dw1 = np.dot(input.T, dh1)
         b1 = np.sum(dh1,axis=0)
 
@@ -134,43 +157,37 @@ class NN (object):
     def update(self, grads, gbias):
 
         #Update ouput layer
-        self.W[2] = self.W[2] - self.learning_rate * grads[2]
-        self.b[2] = self.b[2] - self.learning_rate * gbias[2]
+        self.params['w'][2] = self.params['w'][2] - self.learning_rate * grads[2]
+        self.params['b'][2] =  self.params['b'][2] - self.learning_rate * gbias[2]
 
         #Update last hidden layer
-        self.W[1] = self.W[1] - self.learning_rate * grads[1]
-        self.b[1] = self.b[1] - self.learning_rate * gbias[1]
+        self.params['w'][1] = self.params['w'][1] - self.learning_rate * grads[1]
+        self.params['b'][1] =  self.params['b'][1] - self.learning_rate * gbias[1]
 
         #Update first hidden layer
-        self.W[0] = self.W[0] - self.learning_rate * grads[0]
-        self.b[0] = self.b[0] - self.learning_rate * gbias[0]
+        self.params['w'][0] = self.params['w'][0] - self.learning_rate * grads[0]
+        self.params['b'][0] =  self.params['b'][0] - self.learning_rate * gbias[0]
 
-    def train(self, epochs, weight_init):
-
+    def train(self, epochs, weight_init_method):
         loss_history = []
-
-        #Manual setting of hyperparameters
-        self.batch_size = 32
 
         #Data retrieval
         data = np.load(self.data_path)[0]
         validation = np.load(self.data_path)[1]
 
         #Weight initialization
-        self.initialize_weights(self.input_size, self.num_class, init_type=weight_init)
+        self.initialize_weights(self.input_size, self.num_class, weight_init_method)
 
         #Training
         for epoch in range(0,epochs):
-            #Reinitialize batchsize
-            self.batch_size = 32
             for i in range(0,len(data[0]), self.batch_size):
-
+                current_batch_size = self.batch_size
                 batch_end_index = i + self.batch_size
 
                 #Case where batchsize would cause overflow the data
                 if batch_end_index > len(data[0]):
                     batch_end_index = len(data[0])
-                    self.batch_size = len(data[0]) - i
+                    current_batch_size = len(data[0]) - i
 
                 #Initialize current training batch
                 x = data[0][range(i, batch_end_index)]
@@ -180,22 +197,23 @@ class NN (object):
                 prediction = self.forward(x)
 
                 #Backprop
-                grads, gbias = self.backward(x,t,prediction,self.batch_size)
+                grads, gbias = self.backward(x,t,prediction,current_batch_size)
 
                 #Weight update
                 self.update(grads, gbias)
+
+                #Cache clearing
+                self.cache = []
+
             loss_history.append(np.average(self.l_vector))
             print(np.average(self.l_vector))
             self.l_vector = []
 
-        #self.test(data)
-        #self.test(validation)
-        #plt.plot(loss_history)
-        #plt.show()
+        self.test(validation)
         return loss_history
 
     def test(self, validation_set):
         results = np.sum(np.equal(np.argmax(self.forward(validation_set[0]), axis=1),validation_set[1]).astype(int))
 
-        print (results/len(validation_set[0]))
+        print(results/len(validation_set[0]))
 
